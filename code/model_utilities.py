9915dc070aabf5c2c3a8e8841aebbff2355af938
pouria <pu.moradi@gmail.com>
2019-10-11 13:19:01 +0330

    imlement model utilities; update requirements

--- code/model_utilities.py
+++ code/model_utilities.py
@@ -1,8 +1,40 @@
+import numpy as np
+import cv2
+from keras.models import load_model
 
+model = load_model('../trained_model/model_weight.hdf5')
 
 def predict(img_path):
     """
     :param img_path: str, path of image
     :return: json, prediction on image
     """
-    return {}
+
+    img = cv2.imread(img_path)
+    img = resize_image(img, (224, 224))
+    img = normalize(img)
+
+    result = model.predict(img)
+
+    p = result[0]
+    p = np.sort(p)
+    l = max(float(format(p[0], '.2f')), 1)
+    h = min(float(format(p[-1], '.2f')), 10)
+    m = float(format(p[1], '.2f'))
+    # print("Prediction:" + str(((result[0]))))
+
+    return {'Pessimistic': l, 'Optimistic': h, 'real': m}
+
+
+def resize_image(img, size):
+    img = cv2.resize(img, size)
+    return img
+
+
+def normalize(img):
+    img = np.float64(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0
+    return img
+
+
